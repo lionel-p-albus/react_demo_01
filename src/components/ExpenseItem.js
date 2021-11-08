f2e6bf7af7de9bff137dd75258f534b9ab1ed692
singh_pk <praneed.kla@northbkk.ac.th>
2021-11-08 11:23:07 +0700

    adding basic css styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,12 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
     return (
-        <div>
+        <div className='expense-item'>
             <div>March 28th 2021</div>
-            <div><h2>Car Insurance</h2></div>
-            <div>$249.67</div>
+            <div className="expense-item__description"><h2>Car Insurance</h2>
+                <div className="expense-item__price">$249.67</div>
+            </div>
         </div>
     );
 }
