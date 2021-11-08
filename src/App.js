288e3aa43515dfc3879d710901bfada78a4297ca
singh_pk <praneed.kla@northbkk.ac.th>
2021-11-08 14:34:50 +0700

    an alternative function syntax

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Expenses from "./components/Expenses";
+import Expenses from "./components/expenses/Expenses";
 
-function App() {
+const App = () => {
     const expenses = [
         {id: 'e1', title: 'Toilet Paper', amount: 94.12, date: new Date(2020, 7, 14),},
         {id: 'e2', title: 'Car Insurance', amount: 45.12, date: new Date(2021, 7, 14),},
@@ -9,16 +9,16 @@ function App() {
         {id: 'e4', title: 'Pen', amount: 87.12, date: new Date(2018, 7, 14),},
     ];
 
-    // return (
-    //     <div>
-    //         <h2>Let's get started!</h2>
-    //         <Expenses items={expenses}/>
-    //     </div>
-    // );
-    return React.createElement('div', {},
-        React.createElement('h2', {}, 'Let\'s get started!'),
-        React.createElement(Expenses, {items: expenses}),
+    return (
+        <div>
+            <h2>Let's get started!</h2>
+            <Expenses items={expenses}/>
+        </div>
     );
+    // return React.createElement('div', {},
+    //     React.createElement('h2', {}, 'Let\'s get started!'),
+    //     React.createElement(Expenses, {items: expenses}),
+    // );
 }
 
 export default App;
