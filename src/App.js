d91f816184b50a595ee926cefc71b056fcb750b8
singh_pk <praneed.kla@northbkk.ac.th>
2021-11-11 18:16:41 +0700

    child-to-parent component communication (bottom-up)

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import Expenses from "./components/expenses/Expenses";
 import NewExpense from "./components/newExpense/NewExpense";
 
 const App = () => {
-    const expenses = [
+    let expenses = [
         {id: 'e1', title: 'Toilet Paper', amount: 94.12, date: new Date(2020, 7, 14),},
         {id: 'e2', title: 'Car Insurance', amount: 45.12, date: new Date(2021, 7, 14),},
         {id: 'e3', title: 'Paper', amount: 258.12, date: new Date(2019, 7, 14),},
         {id: 'e4', title: 'Pen', amount: 87.12, date: new Date(2018, 7, 14),},
     ];
 
+    const addExpenseHandler = (expense) => {
+        console.log('In App.js');
+        console.log(expenses);
+        // expenses = {...expenses, expense};
+    }
+
     return (
         <div>
             <h2>Let's get started!</h2>
-            <NewExpense />
+            <NewExpense onAddExpense={addExpenseHandler}/>
             <Expenses items={expenses}/>
         </div>
     );
